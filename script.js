11f9009fd35b4beb4f7730a97471b40baea6db4d
Luz Ponce <luzjazmin2013@gmail.com>
2025-04-28 11:38:53 -0300

    añadi las variables, contsntes y arrays de inicio

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//Constantes de tipos de cafe
+//Constantes, arrays y variables de tipos de cafe
+const tiposDeCafe = ["Expresso", "Americano", "Latte", "Moka", "Capucchino"];
+let preciosDeCafe = [200, 250, 300, 350, 400];
+const tamañosDeCafe = ["Pequeño", "Mediano", "Grande"];
+let precioDeTamaños = [1, 1.5, 2];
+let pedidos = [];
